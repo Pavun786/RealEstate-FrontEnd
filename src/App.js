0720a6bbb8655c49e1c0e0de8e07d071cdca88c5
Pavun786 <pulsarpavuns786@gmail.com>
2024-07-25 12:07:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './Component/Navbar/Navbar';
+import PropertyCard from './Component/Card/PropertyCard';
+import { Route, Routes } from 'react-router-dom';
+import AddProperty from './Component/AddProperty/AddProperty';
+import EditProperty from './Component/EditProperty/EditProperty';
+import Register from './Register';
+import Login from './Login';
+import ProtectedRoute from './ProtectedRoute';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      
+      <Routes>
+        <Route path='/home' element={
+          <ProtectedRoute>
+            <PropertyCard />
+          </ProtectedRoute>
+        } />
+        <Route path='/addnew' element={
+          <ProtectedRoute>
+            <AddProperty />
+          </ProtectedRoute>
+        } />
+        <Route path="/edit/:id" element={<EditProperty />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/' element={<Login />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Global.js
@@ -0,0 +1,1 @@
+export const API = "https://real-estate-backend-pink.vercel.app"
